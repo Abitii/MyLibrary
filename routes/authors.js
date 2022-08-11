555234a5eeccd56978ab46751c3a3134104b7db4
Abitii <aasmero129@gmail.com>
2022-08-11 15:17:24 -0500

    initial Author Route

---
+++ routes/authors.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const { $where } = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All Authors Route
+router.get('/', async(req, res) =>{
+    let searchOptions= {}
+    if (res.query.name != null && req.query.name !==''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render("authors/index", {
+            authors: authors,
+            searchOptions: req.query
+        })
+
+    }catch{
+        res.redirect('/')
+
+
+    }
+    res.render("authors/index")
+})
+
+// New Author Route
+router.get('/new',(req, res) =>{
+    res.render('authors/new', {author: new Author()})
+})
+
+//Create Author Route
+router.post('/', async(req, res) =>{
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save()
+         //res.redirect('authors/$where{newAuthor.id}')
+         res.redirect('authors')
+
+    }catch{
+        res.render('authors/new',{
+            author:author,
+            errorMessage: 'Error creating Author'
+
+        })
+        
+    }
+//     author.save((err, newAuthor)=>{
+//         if (err){
+            // res.render('authors/new',{
+            //     author:author,
+            //     errorMessage: 'Error creating Author'
+            // })
+//         }else{
+//             //res.redirect(authors/$where{newAuthor.id})
+//             res.redirect('authors')
+//         }
+//     })
+   
+ })
+
+module.exports = router
